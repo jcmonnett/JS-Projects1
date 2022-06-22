2fdde267c59653387f517ce9af910be15a08e05a
jcmonnett <17549675+jcmonnett@users.noreply.github.com>
2022-06-21 21:04:43 -0400

    Using Mongoose to enter data into todolist

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,28 +11,62 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to the todolist!"
+});
+
+const item2 = new Item({
+  name: "Press + to add a new item."
+});
+
+const item3 = new Item({
+  name: "Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
+  Item.find({}, function(err, foundItems){
 
-  res.render("list", {listTitle: day, newListItems: items});
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved to database.")
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
+
+  
 
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+
+  res.redirect("/");
 });
 
 app.get("/work", function(req,res){
@@ -45,4 +79,4 @@ app.get("/about", function(req, res){
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
-});
+});r
