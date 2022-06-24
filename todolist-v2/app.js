9b3891a83ced585c1f782599e836919b90da519f
jcmonnett <17549675+jcmonnett@users.noreply.github.com>
2022-06-23 20:26:31 -0400

    Hosted Database and Site setup

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB")
+mongoose.connect("mongodb+srv://USERNAME:PASSWORD@cluster0.les50mn.mongodb.net/todolistDB")
 
 const itemsSchema = {
   name: String
@@ -131,6 +131,12 @@ app.post("/delete", function(req, res){
 app.get("/about", function(req, res){
   res.render("about");
 });
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
