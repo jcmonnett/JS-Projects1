c8e08aad751d6278f73512719dc7cfdba65fa01e
jcmonnett <17549675+jcmonnett@users.noreply.github.com>
2022-07-07 20:17:36 -0400

    MD5

--- Authentication-Secrets-v1/app.js
+++ Authentication-Secrets-v1/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
+
 
 const app = express();
 
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 const secret = process.env.SECRET
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -42,7 +43,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -57,7 +58,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function (req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err) {
